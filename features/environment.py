6d78d9b16e735cb5c543d94403ec6f6fe6cddc07
Ryan Neff <neffry@amazon.com>
2024-05-29 11:06:39 -0400

    variable test variables

--- features/environment.py
+++ features/environment.py
@@ -1,15 +1,31 @@
 import boto3
+import os
+import logging
+import botocore
 
+LOGGER = logging.getLogger()
 
 def before_all(context):
-    context.api_key = ""
+    try:
+        boto3.client('sagemaker').describe_endpoint(
+    		EndpointName='test'
+		)
+        context.endpoint_exists = True
+    except botocore.exceptions.ClientError:
+        context.endpoint_exists = False
+        raise
+        
+        
+    client = boto3.client('apigateway')
+    context.api_key = client.get_api_key(apiKey=os.environ.get('API_KEY_ID'), includeValue=True)['value']
     context.websocket_api_url = (
-        ""
+       f"wss://{os.environ.get('WSS_API_ID')}.execute-api.us-east-1.amazonaws.com/prod/"
     )
     context.rest_api_url = (
-        ""
+        f"https://{os.environ.get('REST_API_ID')}.execute-api.us-east-1.amazonaws.com/prod/"
     )
 
 
 def after_all(context):
-    pass
+    if context.endpoint_exists:
+        dynamo_client = boto3.client("dynamodb")
